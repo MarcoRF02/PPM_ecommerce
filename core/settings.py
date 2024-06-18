a209cb64b536d5f707b493f7a14eecaa2b6ac7a3
MarcoRF02 <marco.rf2002@gmail.com>
2024-06-19 00:02:49 +0200

    1st

--- core/settings.py
+++ core/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/tmp', 'db.sqlite3'),  # Change to /tmp directory
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DATABASE'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),  # Default PostgreSQL port
     }
 }
 
